46a7b302b6b44bc2154ea1659d5dc3eb895bc1c5
Pavel Movchan <ukrolove@gmail.com>
2017-02-06 00:41:00 +0200

    add an ability to eat food

--- main.cpp
+++ main.cpp
@@ -16,7 +16,17 @@ int DIRECTION = DIRECTION_DOWN;
 const int MAP_WIDTH = 40;
 const int MAP_HEIGHT = 15;
 
+// символ части тела змейки
 const char SNAKE_BODY = '*';
+
+// массив доступных символов для еды
+const char FOOD_SYMBOLS[] = {'%', '$', '&', '@', '+'};
+// текущие координаты еды
+int FOOD_X = 0;
+int FOOD_Y = 0;
+
+// номер текущего символа еды
+int FOOD_SYMBOL_NUM = 0;
 
 // одна клетка змейки
 struct snake_body {
@@ -49,16 +59,57 @@ bool is_snake_body(int x, int y) {
     return false;
 }
 
+/// Проверяет, являются ли указанные координаты стенками карты.
+bool is_map_border(int x, int y) {
+    return y == 0 || x == 0 || x == MAP_WIDTH || y == MAP_HEIGHT;
+}
+
+/// Проверяет, находится ли по указанным координатам еда.
+bool is_food(int x, int y) {
+    return FOOD_X == x && FOOD_Y == y;
+}
+
+/// Проверяет, находится ли часть тела змейки на еде.
+bool snake_ate_food() {
+    for (int i(0); i < snake.size(); i++) {
+        if (snake[i].x == FOOD_X && snake[i].y == FOOD_Y) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/// Генерирует новые координаты для еды.
+void generate_food_coord() {
+    srand( time( 0 ) );
+
+    FOOD_X = 3 + rand() % (MAP_WIDTH - 3);
+    FOOD_Y = 3 + rand() % (MAP_HEIGHT - 3);
+}
+
+/// Генерирует новый символ еды.
+void generate_food_symbol() {
+    FOOD_SYMBOL_NUM = rand() % sizeof(FOOD_SYMBOLS);
+}
+
 /// Рисует карту со змейкой.
 void draw() {
     system("clear");
 
+    // рисуем карту с блэкджеком и шлюхами
     for (int i(0); i <= MAP_HEIGHT; i++) {
         for (int j(0); j <= MAP_WIDTH; j++) {
-            if (i == 0 || j == 0 || j == MAP_WIDTH || i == MAP_HEIGHT) {
+
+            if (is_map_border(j, i)) { // проверка на границы карты
                 cout << '#' << flush;
-            } else if (is_snake_body(j, i)) {
+
+            } else if (is_snake_body(j, i)) { // проверка на тело змейки
                 cout << SNAKE_BODY << flush;
+
+            } else if (is_food(j, i)) { // проверка на еду
+                cout << FOOD_SYMBOLS[FOOD_SYMBOL_NUM] << flush;
+
             } else {
                 cout << ' ' << flush;
             }
@@ -74,6 +125,7 @@ void move_snake() {
     int _y = snake[0].y;
     int last_x, last_y;
 
+    // меняем координаты головы
     switch (DIRECTION) {
         case DIRECTION_DOWN: snake[0].y++; break;
         case DIRECTION_LEFT: snake[0].x--; break;
@@ -82,6 +134,8 @@ void move_snake() {
         default:break;
     }
 
+    // каждому следующему элементу тела змейки
+    // присваиваем координаты предыдущего элемента
     for (int i(1); i < snake.size(); i++) {
         last_x = snake[i].x;
         last_y = snake[i].y;
@@ -91,6 +145,20 @@ void move_snake() {
 
         _x = last_x;
         _y = last_y;
+    }
+
+    // если голова змейки находится на тех же координатах что и еда
+    // тогда добавляем часть тела в конец змейки
+    // и генерируем новую еду
+    if (snake_ate_food()) {
+        generate_food_coord();
+        generate_food_symbol();
+
+        snake_body snake_peace;
+        snake_peace.y = snake[0].y;
+        snake_peace.x = snake[0].x;
+
+        snake.push_back(snake_peace);
     }
 }
 
@@ -109,6 +177,9 @@ int main () {
     timeout.tv_usec = 0;
 
     input_on();
+
+    generate_food_coord();
+    generate_food_symbol();
     init_snake();
 
     while (true) {
@@ -116,6 +187,7 @@ int main () {
         FD_ZERO(&rfds);
         FD_SET(STDIN_FILENO, &rfds);
 
+        // если в потоке появились данные
         if (select(1, &rfds, NULL, NULL, &timeout) > 0 && FD_ISSET(STDIN_FILENO, &rfds)) {
             switch (getchar()) {
 
