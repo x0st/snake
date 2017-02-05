bfa81edfdf1468d425dc5b04c967677391dc699b
Pavel Movchan <ukrolove@gmail.com>
2017-02-05 23:00:55 +0200

    create a header file for input control

---
+++ input.h
@@ -0,0 +1,24 @@
+#include <termios.h>
+#include <unistd.h>
+
+struct termios terminalSettings;
+
+void input_on()
+{
+    struct termios newTerminalSettings;
+
+    tcgetattr( STDIN_FILENO, &terminalSettings );
+
+    newTerminalSettings = terminalSettings;
+
+    newTerminalSettings.c_lflag &= ~( ICANON | ECHO );
+    newTerminalSettings.c_cc[VTIME] = 0;
+    newTerminalSettings.c_cc[VMIN] = 1;
+
+    tcsetattr( STDIN_FILENO, TCSANOW, &newTerminalSettings );
+}
+
+void input_off()
+{
+    tcsetattr(STDIN_FILENO, TCSANOW, &terminalSettings);
+}
